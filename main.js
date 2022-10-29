6f9c4629b5cdd2dc8e4635aa9bd1c55216fc36b8
AlfredVeeas <alfredo.veas.a@mail.pucv.cl>
2022-10-28 21:21:32 -0400

    update

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ import data from "./datos.json" assert {type: 'json'}
 
 let producto = document.getElementById('CuerpoArriba');
 
-data.forEach(element => {
-    producto.innerHTML += "<div>" + element.nombreProducto + element.cantidad+"</div>";
+//data.forEach(element => {
+producto.innerHTML += "<div>" + data[0].nombreProducto +"</div>";
+producto.innerHTML += "<div>" + data[0].cantidad +"</div>";
     
     
-})
+//})
