bdb6ee85c181016b89148c1abf0774eeacde909e
AlfredVeeas <alfredo.veas.a@mail.pucv.cl>
2022-10-28 21:41:09 -0400

    update

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 import data from "./datos.json" assert {type: 'json'}
 
-let producto = document.getElementById('CuerpoArriba');
+let cuerpo_arriba = document.getElementById('CuerpoArriba');
 
-//data.forEach(element => {
-producto.innerHTML += "<div>" + data[0].nombreProducto +"</div>";
-producto.innerHTML += "<div>" + data[0].cantidad +"</div>";
+//Cantidad de cuerpo arriba
+cuerpo_arriba.innerHTML += "<div>" + data[0].nombreProducto +"</div>";
+cuerpo_arriba.innerHTML += "<div>" + data[0].cantidad +"</div>";
     
     
-//})
+//cantidad de cuerpo abajo
+let cuerpo_abajo = document.getElementById('CuerpoAbajo');
+
+cuerpo_abajo.innerHTML += "<div>" + data[1].nombreProducto +"</div>";
+cuerpo_abajo.innerHTML += "<div>" + data[1].cantidad +"</div>";
+
+//cantidad de zapatos
+let zapatos = document.getElementById('Zapatos');
+
+zapatos.innerHTML += "<div>" + data[2].nombreProducto +"</div>";
+zapatos.innerHTML += "<div>" + data[2].cantidad +"</div>";
