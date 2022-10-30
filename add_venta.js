f6ca407c47b5a628817235f229d7bc6807e5c0da
AlfredVeeas <alfredo.veas.a@mail.pucv.cl>
2022-10-29 22:36:51 -0400

    update

--- add_venta.js
+++ add_venta.js
@@ -1,38 +1,14 @@
-'use strict';
+import React, {Component} from 'react';
 
-const e = React.createElement;
+class add_venta extends Component{
+  render() {
+    return {
+      <div className="app">
+        <p>
+          HOLAAAAAAAAAAA
+        </p>
+      </div>
 
-class LikeButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-      }
-    
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Name:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        );
-      }
+    };
+  }
 }
-
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
